8f2e6c16b5d7fea840f9ba325506453e5433889c
Soe Than <soethan@better.hr>
2020-07-26 10:45:23 +0630

    Updated

--- app/src/main/java/com/example/daggersamples/MainActivity.java
+++ app/src/main/java/com/example/daggersamples/MainActivity.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
 
+    @Inject
+    Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CarComponent component = DaggerCarComponent.create();
-        Car car = component.getCar();
+        DaggerCarComponent.create().inject(this);
         car.drive();
     }
 }
