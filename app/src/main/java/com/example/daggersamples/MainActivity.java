628cd9a38455b4f4cfd5bad9d5feab3083d6e46e
Soe Than <soethan@better.hr>
2020-07-26 10:41:26 +0630

    delete

--- app/src/main/java/com/example/daggersamples/MainActivity.java
+++ app/src/main/java/com/example/daggersamples/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DaggerCarComponent.create().inject(this);
+        car.drive();
     }
 }
