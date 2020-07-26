6d9728fa003db2ae1b789bc8757c70baa6fd3919
Soe Than <soethan@better.hr>
2020-07-26 11:54:15 +0630

    updated

--- app/src/main/java/com/example/daggersamples/MainActivity.java
+++ app/src/main/java/com/example/daggersamples/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.daggersamples.car.Car;
+
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
